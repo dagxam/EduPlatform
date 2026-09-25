df0077498d11f017db10017630ea9d27291d1b79
dagxam <dagxam@gmail.com>
2026-09-25 15:31:04 +0300

    feat: add logout endpoint

---
+++ api/auth/logout.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+require dirname(__DIR__) . '/bootstrap.php';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    json_response(['ok' => false, 'error' => 'Метод не поддерживается.'], 405);
+}
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'] ?? '', (bool) $params['secure'], (bool) $params['httponly']);
+}
+
+session_destroy();
+json_response(['ok' => true]);
