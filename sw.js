a29ad976a46f4446fd4e0d6559ab1f0957cef4a9
dagxam <dagxam@gmail.com>
2026-09-26 14:12:45 +0300

    pwa: refresh cache for school branding controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uvoria-v9';
+const CACHE = 'uvoria-v10';
 const APP_SHELL = ['./', './index.html', './login.html', './styles.css', './login.css', './app.js', './login.js', './attempt-security.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
