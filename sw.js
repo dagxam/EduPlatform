0fcf7be27a20b02c9782e0d5c89b6e34b457f576
dagxam <dagxam@gmail.com>
2026-09-26 15:41:03 +0300

    pwa: refresh assignment library and school sharing workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uvoria-v13';
+const CACHE = 'uvoria-v14';
 const APP_SHELL = ['./', './index.html', './login.html', './styles.css', './login.css', './app.js', './login.js', './attempt-security.js', './manifest.webmanifest', './favicon.png?v=2'];
 
 self.addEventListener('install', event => {
